06eed208456614f7b89fe15dae2f5cc6cf57edc8
antoalex42-collab <antoalex42@gmail.com>
2026-03-18 12:24:14 +0530

    scene1

---
+++ map.js
@@ -0,0 +1,18 @@
+let arr = [1,2,3,4,5,6];
+let m = arr.map(num => num +1);
+console.log(m);
+
+let prices = ['$400','$500','$600'];
+let repl = prices.map((val) =>
+number(val.replace("$",""))
+)
+console.log(repl);
+
+
+// Accurance
+let str = "nithees";
+let value = spl.reduce((acc,val) => {
+    acc[val] = (acc[val])
+})
+
+//object -> collection of 
